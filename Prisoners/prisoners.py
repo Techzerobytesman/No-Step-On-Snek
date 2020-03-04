502d2496b1172d5405a24c784828b4c5dc45329a
Techzeronbytesman <57227955+Techzeronbytesman@users.noreply.github.com>
2020-03-04 11:09:34 -0600

    h

---
+++ Prisoners/prisoners.py
@@ -0,0 +1,51 @@
+####
+# Each team's file must define four tokens:
+#     team_name: a string
+#     strategy_name: a string
+#     strategy_description: a string
+#     move: A function that returns 'c' or 'b'
+####
+
+team_name = 'DA'
+strategy_name = 'Math time'
+strategy_description = """"\
+If they betrayed in the last four rounds, betray,
+If in the last 6 rounds they colluded betray,
+If we colluded in the last three rounds betray,
+Math to decide twice,
+When in doubt, betray
+"""
+
+import random
+
+
+def move(my_history, their_history, my_score, their_score, opponent_name):
+    """Make my move based on the history with this player.
+
+    history: a string with one letter (c or b) per round that has been played with this opponent.
+    their_history: a string of the same length as history, possibly empty.
+    The first round between these two players is my_history[0] and their_history[0]
+    The most recent round is my_history[-1] and their_history[-1]
+
+    Returns 'c' or 'b' for collude or betray.
+    """
+
+    if 'b' in their_history[-4
+    :]:  # If the other player has betrayed within last 4 rounds,
+        return 'b'  # Betray.
+    else:
+        if 'b' not in their_history [-6]:  # If other player has colluded past 4 rounds
+            return 'b'  # Betray
+        else:
+            if 'c' in my_history[-3]: #If we colluded the past three times
+                return 'b' #Betray
+            else:
+                if my_score >= their_score + 2*their_score: #math
+                    return 'c' #collude
+                else:
+                    if their_score >= my_score + 2*their_score: #math
+                        return 'b' #betray
+                    else:
+                        if 'b' not in their_history[-2]
+                            return 'c' #collude
+                        else: return 'b' and print("Death Before Surrender!")
